ae95d45300cad26d1b566cf9768f9400a2fd114e
Peerapat Junkhaow <peerapat40595@gmail.com>
2019-05-22 20:04:35 +0800

    Add value with service charge

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,19 @@
 <script>
 	let value
 
-	function toNumber(number) {
+	function toNumberFormat(number) {
         return new Intl.NumberFormat(undefined, { style: 'currency', currency: 'SGD' }).format(number)
 	}
 
-	$: valueWithTax = toNumber(value * 1.07)
-	$: valueWithTaxAndService = toNumber(value * 1.07 * 1.10)
+	$: valueWithTax = value * 1.07
+	$: valueWithTaxAndService = valueWithTax * 1.10
+	$: valueWithService = value * 1.10
 </script>
 
 <input type="number" bind:value="{value}">
 <h2>With 7% Tax:</h2>
-<h3>{valueWithTax}</h3>
+<h3>{toNumberFormat(valueWithTax)}</h3>
 <h2>With 7% Tax And 10% Service:</h2>
-<h3>{valueWithTaxAndService}</h3>
+<h3>{toNumberFormat(valueWithTaxAndService)}</h3>
+<h2>With 10% Service:</h2>
+<h3>{toNumberFormat(valueWithService)}</h3>
