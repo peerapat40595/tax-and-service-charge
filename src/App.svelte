26d68305c318421305872981928f014857dd4b44
Peerapat Junkhaow <peerapat40595@gmail.com>
2019-05-22 19:54:37 +0800

    Add logic

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,16 @@
 <script>
-	export let name;
+	let value
+
+	function toNumber(number) {
+        return new Intl.NumberFormat(undefined, { style: 'currency', currency: 'SGD' }).format(number)
+	}
+
+	$: valueWithTax = toNumber(value * 1.07)
+	$: valueWithTaxAndService = toNumber(value * 1.07 * 1.10)
 </script>
 
-<style>
-	h1 {
-		color: purple;
-	}
-</style>
-
-<h1>Hello {name}!</h1>
+<input type="number" bind:value="{value}">
+<h2>With 7% Tax:</h2>
+<h3>{valueWithTax}</h3>
+<h2>With 7% Tax And 10% Service:</h2>
+<h3>{valueWithTaxAndService}</h3>
